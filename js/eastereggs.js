e1207ffa433123360e04a05e386769df71dc0547
GabrielGSD <gabrieldaniel@gec.inatel.br>
2020-03-30 18:36:57 -0300

    Feito a parte do Easter Egg

---
+++ js/eastereggs.js
@@ -0,0 +1,17 @@
+(() => {
+    const $separators = document.querySelectorAll('.separator-dino');
+    let counter = 0;
+
+    $separators.forEach(($separator) => {
+        $separator.addEventListener('click', function(){
+            !this.classList.contains('-clicked') && counter++
+            this.classList.add('-clicked');
+            counter == 4 && showKey()
+        });
+    });
+
+    function showKey(){
+        alert("Mosta Key animada");
+    }
+
+})();
