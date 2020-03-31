013952428d5a60658cbb79c00d40e1d485dba73a
GabrielGSD <gabrieldaniel@gec.inatel.br>
2020-03-31 03:34:13 -0300

    Carrossel finalizado mas com bug de animacao

--- js/components/carousel-dino.js
+++ js/components/carousel-dino.js
@@ -3,6 +3,7 @@
 
     $carouselDino.addEventListener('click', ({target}) => {
         target.parentNode.classList.contains('-left') && clickLeft();
+        target.parentNode.classList.contains('-right') && clickRight();
     })
 
     function clickLeft() {
@@ -13,6 +14,7 @@
 
         const id = parseInt($left.getAttribute('data-id'));
         const nextId = id == 0 ? 14 : id - 1;
+        console.log('ESQUERDA',nextId);
 
         $left.classList.remove('-left');
         $center.classList.remove('-center');
@@ -26,6 +28,30 @@
 
         $left = $carouselDino.querySelector(`[data-id="${nextId}"]`);
         $left.classList.add('-left');
+    }
+
+    function clickRight() {
+
+        let $left = $carouselDino.querySelector('.-left');
+        let $center = $carouselDino.querySelector('.-center');
+        let $right = $carouselDino.querySelector('.-right');
+
+        const id = parseInt($right.getAttribute('data-id'));
+        const backId = id == 14 ? 0 : id + 1;
+        console.log('DIREITA',backId);
+
+        $right.classList.remove('-right');
+        $center.classList.remove('-center');
+        $left.classList.remove('-left');
+
+        $right.classList.add('-center');
+        $center.classList.add('-left');
+
+        $center = $right;
+        $left = $left;
+
+        $right = $carouselDino.querySelector(`[data-id="${backId}"]`);
+        $right.classList.add('-right');
     }
 
 })();
